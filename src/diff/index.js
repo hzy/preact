1651a3177212d4dea1a34c83ca9df6a03fc35088
hzy <28915578+hzy@users.noreply.github.com>
2025-07-09 16:00:36 +0800

    remove unnecessary code

--- src/diff/index.js
+++ src/diff/index.js
@@ -451,10 +451,10 @@ function diffElementNodes(
 
 	if (dom == NULL) {
 		if (nodeType == NULL) {
-			return document.createTextNode(newProps);
+			return options.document.createTextNode(newProps);
 		}
 
-		dom = document.createElementNS(
+		dom = options.document.createElementNS(
 			namespace,
 			nodeType,
 			newProps.is && newProps
